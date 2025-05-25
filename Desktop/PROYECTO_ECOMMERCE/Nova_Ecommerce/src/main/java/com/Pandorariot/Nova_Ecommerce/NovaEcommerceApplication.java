6d052e83fcb6130e45f3e320e52153d9fd75cdcf
PandoraRiot <e.garcia1565@pascualbravo.edu.co>
2025-05-24 21:58:21 -0500

    Initial commit of Nova_Ecommerce project

---
+++ Desktop/PROYECTO_ECOMMERCE/Nova_Ecommerce/src/main/java/com/Pandorariot/Nova_Ecommerce/NovaEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.Pandorariot.Nova_Ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaEcommerceApplication.class, args);
+	}
+
+}
---
+++ Desktop/PROYECTO_ECOMMERCE/Nova_Ecommerce/src/test/java/com/Pandorariot/Nova_Ecommerce/NovaEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pandorariot.Nova_Ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
